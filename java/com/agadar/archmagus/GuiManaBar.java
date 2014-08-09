2ebdd75ac122a6f927be782270288c2c99821839
Martin <Martin@Martin-PC>
2014-08-09 15:27:31 +0200

    Mana bar is working like a charm visually.

--- java/com/agadar/archmagus/GuiManaBar.java
+++ java/com/agadar/archmagus/GuiManaBar.java
@@ -7,13 +7,14 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 
 public class GuiManaBar extends Gui 
 {
-	/** The path to the gui's texture. */
-	private static final ResourceLocation texturepath = new ResourceLocation("archmagus", "textures/gui/mana_bar.png");
+	/** The path to the gui's icons. */
+	private static final ResourceLocation modIcons = new ResourceLocation("archmagus", "textures/gui/archmagus_icons.png");	
 	/** A reference to the Minecraft instance. */
 	private final Minecraft mc;
 	
@@ -22,59 +23,42 @@ public class GuiManaBar extends Gui
 		super();
 		this.mc = mc;
 	}
-
+	
 	@SubscribeEvent(priority = EventPriority.NORMAL)
-	public void onRenderExperienceBar(RenderGameOverlayEvent event)
-	{
+	public void onRenderHungerBar(RenderGameOverlayEvent.Post event)
+	{		
 		if (event.isCancelable() || event.type != ElementType.FOOD)
 			return;
 
+		mc.mcProfiler.startSection("mana");
 		ManaProperties props = ManaProperties.get(this.mc.thePlayer);
-
+		
 		if (props == null || props.getMaxMana() == 0)
 			return;
-
-		// Starting position for the mana bar - 2 pixels from the top left corner.
-		int xPos = 2;
-		int yPos = 2;
-
+		
+		mc.getTextureManager().bindTexture(modIcons);
 		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_DEPTH_TEST);
-		GL11.glDepthMask(false);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		int width = event.resolution.getScaledWidth();
+        int height = event.resolution.getScaledHeight();
+		int left = width / 2 + 91;
+        int top = height - GuiIngameForge.right_height;
+        int currentMana = props.getCurrentMana();
+        
+        for (int i = 0; i < 10; ++i)
+        {
+            int idx = i * 2 + 1;
+            int x = left - i * 8 - 9;
+            int y = top;
 
-		// Somewhere in Minecraft vanilla code it says to do this because of a lighting bug
-		GL11.glDisable(GL11.GL_LIGHTING);
-
-		// Bind your texture to the render engine
-		this.mc.getTextureManager().bindTexture(texturepath);
-
-		/*
-		The parameters for drawTexturedModalRect are as follows:
-
-		drawTexturedModalRect(int x, int y, int u, int v, int width, int height);
-
-		x and y are the on-screen position at which to render.
-		u and v are the coordinates of the most upper-left pixel in your texture file from which to start drawing.
-		width and height are how many pixels to render from the start point (u, v)
-		*/
-		// First draw the background layer. In my texture file, it starts at the upper-
-		// left corner (x=0, y=0), ends at 50 pixels (so it's 51 pixels long) and is 4 pixels thick (y value)
-		this.drawTexturedModalRect(xPos, yPos, 0, 0, 51, 4);
-		// Then draw the foreground; it's located just below the background in my
-		// texture file, so it starts at x=0, y=4, is only 2 pixels thick and 49 length
-		// Why y=4 and not y=5? Y starts at 0, so 0,1,2,3 = 4 pixels for the background
-
-		// However, we want the length to be based on current mana, so we need a new variable:
-		int manabarwidth = (int)(((float) props.getCurrentMana() / props.getMaxMana()) * 49);
-		//System.out.println("[GUI MANA] Current mana bar width: " + manabarwidth);
-		// Now we can draw our mana bar at yPos+1 so it centers in the background:
-		this.drawTexturedModalRect(xPos, yPos + 1, 0, 4, manabarwidth, 2);
-		// NOTE: be sure to reset the openGL settings after you're done or your character model will be messed up
-		GL11.glDisable(GL11.GL_BLEND);
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glDepthMask(true);
+            this.drawTexturedModalRect(x, y, 0, 0, 9, 9);
+            
+            if (idx < currentMana)
+            	this.drawTexturedModalRect(x, y, 36, 0, 9, 9);
+            else if (idx == currentMana)
+            	this.drawTexturedModalRect(x, y, 45, 0, 9, 9);
+        }
+        
+        GL11.glDisable(GL11.GL_BLEND);
+        mc.mcProfiler.endSection();
 	}
 }
